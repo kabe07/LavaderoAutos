a195b5f81818137e9b993284eed5425caf1d7b00
Martin Fernandez <martinfernandezminni@gmail.com>
2026-09-21 20:25:59 -0300

    comenzando a escribir codigo

---
+++ src/main/java/com/lavadero/lavadero_api/LavaderoApiApplication.java
@@ -0,0 +1,13 @@
+package com.lavadero.lavadero_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavaderoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavaderoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lavadero/lavadero_api/LavaderoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavadero.lavadero_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavaderoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
